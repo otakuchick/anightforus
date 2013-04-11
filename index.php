e7d3430171f839344fdb6d3b0bab5996a0c390dc
Natalie Greco <natalie@koibot.com>
2013-04-10 23:51:55 -0700

    getting this code onto github for safe keeping.

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>A Night For Us - Rachel and Natalie</title>
+<!--
+/* @license
+ * MyFonts Webfont Build ID 1763683, 2011-12-20T13:57:21-0500
+ * 
+ * The fonts listed in this notice are subject to the End User License
+ * Agreement(s) entered into by the website owner. All other parties are 
+ * explicitly restricted from using the Licensed Webfonts(s).
+ * 
+ * You may obtain a valid license at the URLs below.
+ * 
+ * Webfont: Populaire Light by PintassilgoPrints
+ * URL: http://www.myfonts.com/fonts/pintassilgo/populaire/light/
+ * Licensed pageviews: 10,000
+ * 
+ * Webfont: Populaire Regular by PintassilgoPrints
+ * URL: http://www.myfonts.com/fonts/pintassilgo/populaire/regular/
+ * Licensed pageviews: unspecified
+ * 
+ * 
+ * License: http://www.myfonts.com/viewlicense?type=web&buildid=1763683
+ * Webfonts copyright: By PintassilgoPrints, 2011. All rights reserved.
+ * www.pintassilgoprints.com
+ * 
+ * © 2011 Bitstream Inc
+*/
+
+-->
+<link href="favicon.ico" type="image/ico" rel="shortcut icon">
+<link href="favicon.png" type="image/png" rel="shortcut icon">
+<link href="apple-icon.png" type="image/png" rel="apple-touch-icon">
+<link rel="stylesheet" href="MyFontsWebfontsOrderM3382893.css">
+<link rel="stylesheet" href="main.css">
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-1803872-3']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+<body class="birds">
+	<div class="content">
+		<h1 class="Populaire">January 28th, A Night For Us</h1>
+		<p>Rachel and Natalie invite you to join in the celebration as they exchange vows and party the night away.</p>
+		<img src="images/couple.png" width="500px" height="166px" />
+		
+		<h2 class="Populaire">Where</h2>
+		<p>Sole Repair Shop<br />
+		<a href="http://g.co/maps/wz55w">1452 10th Ave<br />
+		Seattle, WA 98122</a></p>
+		<p>As scandalous and elusive as it sounds, you need to look for the white door. There are no signs or an address out front, but there is a door man.</p>
+		<p>Please plan to arrive at 6pm sharp. Ceremony will begin shortly after. Reception to follow.</p>
+		<img src="images/street.png" width="500px" height="166px" />
+		
+		<h2 class="Populaire">What to Expect</h2>
+		<p>The ceremony will be held downstairs. There will be some reserved seating for family up front, the rest will be open. We also encourage those who don't mind standing to head upstairs to the loft for a better view of the ceremony. We will love you forever if you take a lot of photos/video.</p>
+		<p>Following the ceremony, downstairs will be transformed into dancing and karaoke. We encourage you to sing as many karaoke songs as possible, bonus points for upbeat and danceable, or super cheesy and romantic.</p>
+		<p>If karaoke isn't your thing upstairs will be the place for you. More chill and lounge feeling, with an Xbox/Kinect playing Dance Central and Michael Jackson Experience.</p>
+		<img src="images/venue.png" width="500px" height="166px" />
+		
+		<h2 class="Populaire">Parking</h2>
+		<p>Parking is immediately to the south of the venue. It is a pay lot that accepts credit/debit cards. You may get lucky and find street parking, but I wouldn't count on it. If you live close, a bus or cab may be your best bet. If you're feeling fancy, there is always <a href="http://www.uber.com">Uber Car</a> or <a href="http://www.crownblackcar.com">Crown Black Car Service</a>.</p>
+		<img src="images/parking.png" width="500px" height="166px" />
+		
+		<h2 class="Populaire">Food + Drink</h2>
+		<p>There will be tasty hors d'oeuvres, and partially hosted beer + wine. Should you have a taste for mixed drinks there will be a cash bar. We encourage you to eat dinner prior to arriving. Also, there is a rumor there will be a midnight run to Dick's Burgers.</p>
+		<img src="images/food.png" width="500px" height="166px" />
+		
+		<h2 class="Populaire">Kids</h2>
+		<p>Kids are absolutely welcome and encouraged to attend, just keep in mind that anyone under 21 needs to leave by 10pm. So if you want to party the night away, hiring a sitter may be a good idea.</p>
+		<img src="images/kids.png" width="500px" height="166px" />
+		
+		<h2 class="Populaire">Registry</h2>
+		<p>In an attempt to move away from the norm, family and friends are encouraged to <a href="https://www.paypal.com/us/cgi-bin/webscr?cmd=_send-money-transfer&send_method=domestic&nav=1.2.0">donate to the Honeymoon Fund</a>. The plan is to spend our time relaxing in Hawaii (we've never been!) On the to-do list: hiking, volcano tours, surfing, etc. Also taking suggestions!</p>
+		<p><a href="https://www.paypal.com/us/cgi-bin/webscr?cmd=_send-money-transfer&send_method=domestic&nav=1.2.0">PayPal</a> is the easiest, most familiar way to send money online. We encourage you to use the "other" option under personal payments to avoid charges. Email address is natalie@otakuchick.com.</p>
+		<p>If you would rather send a more traditional gift, we are registered at <a href="http://www.crateandbarrel.com/Gift-Registry/Guest/View-Registry.aspx?grid=4828776">Crate and Barrel</a>.</p>
+		<img src="images/registry.png" width="500px" height="166px" />
+		
+		<h2 class="Populaire">Hotels</h2>
+		<p>We know a lot of you are coming in from out of town. Below are a few hotel suggestions. We can't personally vouch for these hotels, but they seem to be in decent areas and fairly priced. Feel free to also check <a href="http://www.airbnb.com">airbnb.com</a> for deals, Capital Hill would be a great neighborhood to check, as well as downtown or Queen Anne.</p>
+		
+		<p><a href="http://www.hotels.com/hotel/details.html?pa=11&pn=1&ps=11&tab=description&destinationId=1642722&hotelId=150213&arrivalDate=01-28-12&departureDate=01-29-12&rooms[0].numberOfAdults=1&roomno=1&validate=false&previousDateful=false&reviewOrder=date_newest_first#description">Holiday Inn Express Hotel & Suites Seattle - City Center</a></p>
+		<p><a href="http://www.hotels.com/hotel/details.html?pa=5&pn=1&ps=5&tab=description&destinationId=1642722&hotelId=206005&arrivalDate=01-28-12&departureDate=01-29-12&rooms%5B0%5D.numberOfAdults=1&roomno=1&validate=false&previousDateful=false&reviewOrder=date_newest_first#description">Mediterranean Inn</a></p>
+		<p><a href="http://www.hotels.com/hotel/details.html?pa=7&pn=1&ps=7&tab=description&destinationId=1642722&hotelId=132684&arrivalDate=01-28-12&departureDate=01-29-12&rooms%5B0%5D.numberOfAdults=1&roomno=1&validate=false&previousDateful=false&reviewOrder=date_newest_first#description">Executive Hotel Pacific</a></p>
+		<p><a href="http://www.hotels.com/hotel/details.html?pa=6&pn=1&ps=6&tab=description&destinationId=1642722&hotelId=112304&arrivalDate=01-28-12&departureDate=01-29-12&rooms%5B0%5D.numberOfAdults=1&roomno=1&validate=false&previousDateful=false&reviewOrder=date_newest_first#description">La Quinta Inn and Suites Seattle Downtown</a></p>
+		<p><a href="http://www.hotels.com/hotel/details.html?pa=2&pn=1&ps=2&tab=description&destinationId=1642722&hotelId=117308&arrivalDate=01-28-12&departureDate=01-29-12&rooms%5B0%5D.numberOfAdults=1&roomno=1&validate=false&previousDateful=false&reviewOrder=date_newest_first#description">Quality Inn & Suites Hotel Seattle Center</a></p>
+		
+		<h2 class="Populaire">We look forward to seeing you!</h2>
+		<p class="end">Original artwork by the awesome Katie Greff.</p>
+	</div>
+</body>
+</html>
